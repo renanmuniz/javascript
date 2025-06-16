98eed0b0980e9b0f975eb2682423d10625ad727d
renan muniz <renan.mmerlo@gmail.com>
2025-06-16 09:06:18 -0300

    ate aula 8 - variables

---
+++ js-basics/index.js
@@ -0,0 +1,12 @@
+//Comentário em JS.
+console.log('Hello World JS!');
+
+//Variables
+let firstName = 'Renan';
+let lastName = 'Muniz'
+console.log(firstName + ' ' + lastName);
+
+let n1 = 2;
+let n2 = 3;
+let sum = n1 + n2;
+console.log(sum);
