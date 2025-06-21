2d985caa9b828bc7e95e6f774c41ccb96d96af5f
renan muniz <renan.mmerlo@gmail.com>
2025-06-21 12:26:28 -0300

    Curso JS ate aula 13 Arrays

--- js-basics/index.js
+++ js-basics/index.js
@@ -1,12 +1,32 @@
-//Comentário em JS.
-console.log('Hello World JS!');
+let selectedColors = ['red', 'green'];
+console.log(selectedColors); //[ 'red', 'green' ]
+console.log(selectedColors[0]); //red
+console.log(selectedColors[1]); //green
+console.log(selectedColors[2]); //undefined
 
-//Variables
-let firstName = 'Renan';
-let lastName = 'Muniz'
-console.log(firstName + ' ' + lastName);
+//Adicionando itens ao array:
+//Adicionando ao final com push:
+selectedColors.push('blue');
+console.log(selectedColors); //[ 'red', 'green', 'blue' ]
 
-let n1 = 2;
-let n2 = 3;
-let sum = n1 + n2;
-console.log(sum);
+//Adicionando no começo do array:
+selectedColors.unshift('yellow');
+console.log(selectedColors); //[ 'yellow', 'red', 'green', 'blue' ]
+
+//Adicionando em uma posição específica com .splice:
+selectedColors.splice(1, 1, 'brown'); //Posicao:1 RemoveAtual:1 Valor:'brown'
+console.log(selectedColors); //[ 'yellow', 'brown', 'green', 'blue' ]
+
+selectedColors.splice(1, 0, 'purple'); //Posicao:1 RemoveAtual:0 Valor:'purple'
+console.log(selectedColors); //[ 'yellow', 'purple', 'brown', 'green', 'blue' ] 
+
+//Adicionando em posição específica pelo índice(substitui se já tiver valor nele):
+selectedColors[5] = 'black'
+console.log(selectedColors); //[ 'yellow', 'purple', 'brown', 'green', 'blue', 'black' ]
+
+selectedColors[2] = 'white'
+console.log(selectedColors); //[ 'yellow', 'purple', 'white', 'green', 'blue', 'black' ]
+
+//Adicionando pulando psoições preenchidas:
+selectedColors[7] = 'gray'
+console.log(selectedColors); //['yellow','purple','white','green','blue','black',<1 empty item>,'gray']
